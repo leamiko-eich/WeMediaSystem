2613488e0a2dd6df1e7d1cbabb460ddc2d7441c8
yjfdl123 <yjfdl123@163.com>
2023-08-19 22:16:49 +0800

    update

--- commonlib/media_interface/selenium_spiders/DouyinSelenium.py
+++ commonlib/media_interface/selenium_spiders/DouyinSelenium.py
@@ -78,6 +78,35 @@ class DouyinSelenium(BaseSelenium):
         return chrome_options
 
 
+    def publish_video(self):
+        path_video = 'H:/6-2.MP4'
+        chrome_options = self.get_chrome_options()
+
+        driver = self.driver
+        print("type driver:", type(driver))
+        driver : webdriver.Chrome = driver
+
+        ## 打开图文发布界面
+        url = 'https://creator.douyin.com/creator-micro/content/upload'
+
+        driver.get(url)
+        time.sleep(15)
+
+        ele_box = driver.find_element(By.CLASS_NAME, 'container--1GAZf')
+        self.save_element_html(ele_box)
+
+        input_element = driver.find_element(By.CSS_SELECTOR, 'input[type="file"]')
+        # 输入文件路径到<input>元素
+        file_path = path_video
+        input_element.send_keys(file_path)
+        time.sleep(2)
+
+        
+        
+        time.sleep(50)
+        return
+
+
     def publish_article(self, article_title, article_content):
         chrome_options = self.get_chrome_options()
 
@@ -85,119 +114,41 @@ class DouyinSelenium(BaseSelenium):
         print("type driver:", type(driver))
         driver : webdriver.Chrome = driver
 
+        ## 打开图文发布界面
+        url = 'https://creator.douyin.com/creator-micro/content/upload?default-tab=3'
+        driver.get(url)
 
+        time.sleep(15)
+
+        
+        # 找到上传区域
+        upload_div = driver.find_element(By.CSS_SELECTOR, "div.upload--nCmEF") 
+        self.save_element_html(upload_div, 'upload_div.html')
+
+        # 已有的 input 标签
+        # input_element = upload_div.find_element(By.CLASS_NAME, "box-s0--2-vRb")
+        # input_element = upload_div.find_element(By.CLASS_NAME, "info-desc--1WX-f")
+        input_element = upload_div.find_element(By.CLASS_NAME, "desc--2WwkF")
+        self.save_element_html(input_element,'input_element.html')
+
+        # 将文件路径传给 input 的 value 属性
+        path_image ="H:/2.jpeg"
+        input_element.send_keys(path_image) 
+
+        # 可以上传多张图片
+        #input_element.send_keys("/path/to/image2.png")
+
+
+
+        time.sleep(30)
+        return 
         ## 点击投稿按钮
         button_write_article = driver.find_element(By.CLASS_NAME, 'header-upload-entry__text')
         self.save_element_html(button_write_article, 'button_write_article.html')
         button_write_article.click()
 
         
-        ## 切换到新窗口
-        print("prev url:",  driver.current_url)
-        self.switch_to_new_windows(driver)
-        print("after url:",  driver.current_url)
-        self.save_driver_html(driver, 'driver.html')
 
-
-        nav_bar = driver.find_element(By.CLASS_NAME, "upload-nav")  
-
-        # 根据链接文本内容定位到"专栏投稿"链接
-        column_link = nav_bar.find_element(By.LINK_TEXT, "专栏投稿")
-        column_link.click()
-        time.sleep(2)
-
-        self.save_driver_html(driver, 'driver.html')
-
-        
-        ## 寻找输入的iframe
-        iframe_box = driver.find_element(By.ID, 'edit-article-box')
-        self.save_element_html(iframe_box, 'iframe_box.html')
-        time.sleep(2)
-
-        ## 先获取iframe元素
-        iframe = iframe_box.find_element(By.CSS_SELECTOR, "iframe")
-        self.save_element_html(iframe, 'iframe_before.html')
-
-        # 切换到iframe 内部 
-        print("切换到内部")
-        driver.switch_to.frame(iframe)
-
-        # 等待iframe加载完成
-        time.sleep(5) 
-        self.save_driver_html(driver, 'iframe_innder.html')
-
-        ## 输入标题
-        input_title = driver.find_element(By.CSS_SELECTOR, '.ui-input-textarea.article-title')
-        self.save_element_html(input_title, 'input_title.html')
-        textarea = input_title.find_element(By.CSS_SELECTOR, 'textarea')
-        textarea.send_keys(article_title)
-        time.sleep(2)
-
-        ## 输入内容- 获取iframe
-        # iframe_content = driver.find_element(By.CSS_SELECTOR, '.notranslate.public-DraftEditor-content')
-        iframe_content = driver.find_element(By.ID, 'ueditor_0')
-        self.save_element_html(iframe_content, 'iframe_content_outer.html')
-        # iframe_content.send_keys(content)
-        # driver.execute_script("arguments[0].value = '%s'" % (article_content), iframe_content)
-
-        print("切换到内部 -content")
-        driver.switch_to.frame(iframe_content)
-        time.sleep(5)
-        self.save_driver_html(driver, 'iframe_content_inner.html')
-
-
-        ## 寻找插入框
-        body_content = driver.find_element(By.CSS_SELECTOR, "body")
-        self.save_element_html(body_content, 'body_content.html')
-
-        # body_content.clear()
-        body_content.send_keys(article_content)
-        time.sleep(3)
-
-
-        ## 切回到ifram1
-        driver.switch_to.parent_frame()  
-        self.save_driver_html(driver, 'main_iframe1.html')
-        time.sleep(2)
-
-        ## 最后提交按钮
-        bnt_final_submit = driver.find_element(By.CSS_SELECTOR, '.ui-btn.blue-radius')
-        self.save_element_html(bnt_final_submit, 'bnt_final_submit.html')
-        bnt_final_submit.click()
-        time.sleep(2)
-
-        ## 切回到主页面
-        driver.switch_to.default_content()
-        self.save_driver_html(driver, 'main_default.html')
-        
-
-        time.sleep(60)
-        return
-
-        time.sleep(15)
-
-        
-
-        # iframe_content = driver.find_element(By.CLASS_NAME, 'public-DraftStyleDefault-block public-DraftStyleDefault-ltr')
-
-
-        btn_publish = driver.find_element(By.CLASS_NAME, 'editor-extra-button-submit')
-        btn_publish.click()
-        time.sleep(2)
-
-        box_publish = driver.find_element(By.CLASS_NAME, 'editor-popup-setting-submit')
-        self.save_element_html(box_publish,'box_publish.html')
-
-        final_pub_btn = box_publish.find_element(By.CSS_SELECTOR, 'button[type="submit"]')
-        self.save_element_html(final_pub_btn,'final_pub_bnt.html')
-        final_pub_btn.click()
-
-
-
-        
-
-        time.sleep(30)
-        return
         
         
 if __name__ == "__main__":
@@ -206,5 +157,5 @@ if __name__ == "__main__":
     content ="Good Good Study, Day Day Up. 是的"
     username = '18511400319'
     # douyin_selenium.login_with_password(username)
-    driver = douyin_selenium.login_with_cookie(username, wait_time=60)
-    # douyin_selenium.publish_article(title, content)
+    driver = douyin_selenium.login_with_cookie(username, wait_time=10)
+    douyin_selenium.publish_article(title, content)
