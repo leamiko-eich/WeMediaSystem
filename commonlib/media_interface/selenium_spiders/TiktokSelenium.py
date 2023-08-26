405ecbd6cd621c7ecd8af75955423fd1c65a9386
yjfdl123 <yjfdl123@163.com>
2023-08-26 11:50:40 +0800

    add tiktok publish

--- commonlib/media_interface/selenium_spiders/TiktokSelenium.py
+++ commonlib/media_interface/selenium_spiders/TiktokSelenium.py
@@ -77,46 +77,31 @@ class TiktokSelenium(BaseSelenium):
         chrome_options.add_argument('--disable-infobars')
         return chrome_options
 
+    def publish_video(self):
+        ## input参数
+        path_video = 'H:/6-2.MP4'
+        title_video  ='今天说点什么'
 
-    def publish_article(self, article_title, article_content):
+        url_upload = 'https://www.tiktok.com/upload?lang=zh-CN'
+
+        ## output
         chrome_options = self.get_chrome_options()
-
         driver = self.driver
-        print("type driver:", type(driver))
         driver : webdriver.Chrome = driver
 
+        print("第1次刷新upload")
+        driver.get(url_upload)
+        time.sleep(10)
 
-        ## 点击投稿按钮
-        button_write_article = driver.find_element(By.CLASS_NAME, 'header-upload-entry__text')
-        self.save_element_html(button_write_article, 'button_write_article.html')
-        button_write_article.click()
-
-        
-        ## 切换到新窗口
-        print("prev url:",  driver.current_url)
-        self.switch_to_new_windows(driver)
-        print("after url:",  driver.current_url)
-        self.save_driver_html(driver, 'driver.html')
-
-
-        nav_bar = driver.find_element(By.CLASS_NAME, "upload-nav")  
-
-        # 根据链接文本内容定位到"专栏投稿"链接
-        column_link = nav_bar.find_element(By.LINK_TEXT, "专栏投稿")
-        column_link.click()
-        time.sleep(2)
-
-        self.save_driver_html(driver, 'driver.html')
-
-        
-        ## 寻找输入的iframe
-        iframe_box = driver.find_element(By.ID, 'edit-article-box')
-        self.save_element_html(iframe_box, 'iframe_box.html')
-        time.sleep(2)
+        print("第2次刷新upload")
+        driver.get(url_upload)
+        time.sleep(30)
+        self.save_driver_html(driver, 'tiktok_upload.html')
 
         ## 先获取iframe元素
-        iframe = iframe_box.find_element(By.CSS_SELECTOR, "iframe")
+        iframe = driver.find_element(By.CSS_SELECTOR, "iframe")
         self.save_element_html(iframe, 'iframe_before.html')
+        time.sleep(20)
 
         # 切换到iframe 内部 
         print("切换到内部")
@@ -126,76 +111,57 @@ class TiktokSelenium(BaseSelenium):
         time.sleep(5) 
         self.save_driver_html(driver, 'iframe_innder.html')
 
-        ## 输入标题
-        input_title = driver.find_element(By.CSS_SELECTOR, '.ui-input-textarea.article-title')
-        self.save_element_html(input_title, 'input_title.html')
-        textarea = input_title.find_element(By.CSS_SELECTOR, 'textarea')
-        textarea.send_keys(article_title)
-        time.sleep(2)
+        ## 找到上传按钮
+        div_container = driver.find_element(By.CSS_SELECTOR, '.jsx-4163898440.upload-container')
+        self.save_element_html(div_container, 'div_container.html')
 
-        ## 输入内容- 获取iframe
-        # iframe_content = driver.find_element(By.CSS_SELECTOR, '.notranslate.public-DraftEditor-content')
-        iframe_content = driver.find_element(By.ID, 'ueditor_0')
-        self.save_element_html(iframe_content, 'iframe_content_outer.html')
-        # iframe_content.send_keys(content)
-        # driver.execute_script("arguments[0].value = '%s'" % (article_content), iframe_content)
+        file_input = div_container.find_element(By.XPATH, "//input[@type='file' and @accept='video/*']")
+        self.save_element_html(file_input, 'file_input.html')
 
-        print("切换到内部 -content")
-        driver.switch_to.frame(iframe_content)
-        time.sleep(5)
-        self.save_driver_html(driver, 'iframe_content_inner.html')
+        ## Upload 按钮
+        file_path = path_video
+        file_input.send_keys(file_path)
+        print("\t 等待视频加载完毕， 30 S")
+        time.sleep(30)
+        self.save_driver_html(driver, 'tic_info_after.html')
+        # time.sleep(120)
 
 
-        ## 寻找插入框
-        body_content = driver.find_element(By.CSS_SELECTOR, "body")
-        self.save_element_html(body_content, 'body_content.html')
 
-        # body_content.clear()
-        body_content.send_keys(article_content)
-        time.sleep(3)
+        ## 上传视频
+        print("开始上传视频")
 
-
-        ## 切回到ifram1
-        driver.switch_to.parent_frame()  
-        self.save_driver_html(driver, 'main_iframe1.html')
-        time.sleep(2)
-
-        ## 最后提交按钮
-        bnt_final_submit = driver.find_element(By.CSS_SELECTOR, '.ui-btn.blue-radius')
-        self.save_element_html(bnt_final_submit, 'bnt_final_submit.html')
-        bnt_final_submit.click()
-        time.sleep(2)
-
-        ## 切回到主页面
-        driver.switch_to.default_content()
-        self.save_driver_html(driver, 'main_default.html')
         
+        ## 预约发布
+        print("预约发布")
+        div_schedule = driver.find_element(By.CSS_SELECTOR, '.jsx-3471246984.scheduled-container')
+        btn_schedule = div_schedule.find_element(By.ID, 'tux-3')
+        btn_schedule.click()
+        time.sleep(5)
 
-        time.sleep(60)
+        ## 填写title
+        print("修改title")
+        div_title_container = driver.find_element(By.CSS_SELECTOR, '.jsx-1768246377.container')
+        div_editor = div_title_container.find_element(By.CSS_SELECTOR, '.DraftEditor-editorContainer')
+        # div_editor.send_keys("hhhh")
+        time.sleep(5)
+
+        
+        ## 发布
+        btn_submit = driver.find_element(By.CSS_SELECTOR, '.jsx-3366794632.btn-post')       
+        btn_submit.click()
+
+
+        
+        time.sleep(30)
         return
 
-        time.sleep(15)
+    def publish_article(self, article_title, article_content):
+        chrome_options = self.get_chrome_options()
 
-        
-
-        # iframe_content = driver.find_element(By.CLASS_NAME, 'public-DraftStyleDefault-block public-DraftStyleDefault-ltr')
-
-
-        btn_publish = driver.find_element(By.CLASS_NAME, 'editor-extra-button-submit')
-        btn_publish.click()
-        time.sleep(2)
-
-        box_publish = driver.find_element(By.CLASS_NAME, 'editor-popup-setting-submit')
-        self.save_element_html(box_publish,'box_publish.html')
-
-        final_pub_btn = box_publish.find_element(By.CSS_SELECTOR, 'button[type="submit"]')
-        self.save_element_html(final_pub_btn,'final_pub_bnt.html')
-        final_pub_btn.click()
-
-
-
-        
-
+        driver = self.driver
+        print("type driver:", type(driver))
+        driver : webdriver.Chrome = driver
         time.sleep(30)
         return
         
@@ -206,5 +172,10 @@ if __name__ == "__main__":
     content ="Good Good Study, Day Day Up. 是的"
     username = '18511400319'
     # tiktok_selenium.login_with_password(username, wait_time=100)
-    driver = tiktok_selenium.login_with_cookie(username, wait_time=30)
+    driver = tiktok_selenium.login_with_cookie(username, wait_time=10)
     # tiktok_selenium.publish_article(title, content)
+
+    tiktok_selenium.publish_video()
+
+    
+    tiktok_selenium.quit_driver()
